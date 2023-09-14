3b9a43cc4a8f6493f14ca4fd840596d9d044d2e5
djadbgks <129721879+djadbgks@users.noreply.github.com>
2023-09-14 16:13:37 +0900

    Add files via upload

--- dino_game.js
+++ dino_game.js
@@ -11,12 +11,49 @@ let obstacles = []; // 장애물
 let gameSpeed; // 게임 속도
 let keys = {}; // 키 값
 
+let tempSeoul = 0;
+
 //이벤트 리스너 추가
 document.addEventListener("keydown", function (evt) {
   keys[evt.code] = true;
 });
 document.addEventListener("keyup", function (evt) {
   keys[evt.code] = false;
+});
+
+const getJSON = function(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        const status = xhr.status;
+        if(status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+getJSON('https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=ef2938c51671994649928192d6aebd7b&units=metric'
+,
+function(err, data) {
+    if(err !== null) {
+        alert('예상치 못한 오류 발생.' + err);
+    } else {
+        tempSeoul = data.main.temp;
+        alert(`현재
+        온도는 ${data.main.temp}°
+        풍속은 ${data.wind.speed}m/s
+        습도는 ${data.main.humidity}%
+        
+      입니다.
+      오늘의 
+        최고기온은 ${data.main.temp_max}°
+        최저기온은 ${data.main.temp.min}°
+    입니다.`)
+    }
 });
 
 class Dino {
@@ -40,8 +77,14 @@ class Dino {
       img.src = "./img/dino_down.png";
       ctx.drawImage(img, this.x, this.y, this.w, this.h);
     } else {
-      img.src = "./img/dino_up.png";
-      ctx.drawImage(img, this.x, this.y, this.w, this.h);
+        if(tempSeoul < 15)
+        {
+            img.src = './img/santa_dino_up.png';
+            ctx.drawImage(img, this.x, this.y, this.w, this.h);
+        }else {
+            img.src = './img/dino_up.png';
+            ctx.drawImage(img, this.x, this.y, this.w, this.h);
+        }
     }
   }
 
